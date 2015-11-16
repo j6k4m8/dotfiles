5602f4512c7fc199bcec25de95cc62930d00fb99
Jordan Matelsky <j6k4m8@gmail.com>
2015-11-16 16:37:51 -0500

    Create server-starter.sh

---
+++ server-starter.sh
@@ -0,0 +1,11 @@
+# First, we need to install a few things...
+sudo apt-get install zsh mosh htop tmux
+
+# Now install oh-my-zsh.
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Now download a vimrc.
+wget https://raw.githubusercontent.com/j6k4m8/dotfiles/master/.vimrc-default -o ~/.vimrc
+
+# The same with a tmux.conf:
+wget https://raw.githubusercontent.com/j6k4m8/dotfiles/master/.tmux.conf -o ~/.tmux.conf
