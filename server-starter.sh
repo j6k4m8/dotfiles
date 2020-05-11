b0adb997743800ec3a93736a32c6b6abc0a8de89
Jordan Matelsky <j6k4m8@users.noreply.github.com>
2020-05-11 11:09:40 -0400

    Update server-starter.sh

--- server-starter.sh
+++ server-starter.sh
@@ -1,9 +1,9 @@
 # Install some tools
-sudo apt-get install -y zsh tmux htop wget mosh git
+sudo apt install -y zsh tmux htop wget mosh git
 
-# Configure git... disables
-#git config --global user.name "Jordan Matelsky"
-#git config --global user.email "jordan@matelsky.com"
+# Configure git... disabled
+git config --global user.name "Jordan Matelsky"
+git config --global user.email "jordan@matelsky.com"
 
 # Download vimrc and tmux config
 wget https://raw.githubusercontent.com/j6k4m8/dotfiles/master/.tmux.conf -O ~/.tmux.conf
@@ -12,5 +12,3 @@ wget https://raw.githubusercontent.com/j6k4m8/dotfiles/master/.vimrc -O ~/.vimrc
 # Install oh-my-zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
 
-# Install meteor... disabled.
-# curl https://install.meteor.com | sh
